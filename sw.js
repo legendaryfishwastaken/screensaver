fa40d3f498d57e1c4b401eba682cadbfd618ecba
legendaryfishwastaken <129801050+legendaryfishwastaken@users.noreply.github.com>
2024-03-11 14:15:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('wallpaper-rotator-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/sw.js',
-        // Add more files to cache here
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
 });
